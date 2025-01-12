601ed37cc8a78c7f7f80a9da909e6174370a8c74
Anuj Kumar Kheria <anujkheria1@gmail.com>
2025-01-12 09:41:37 +0530

    API Intergration

--- src/Pages/Auth/ForgotPassword.tsx
+++ src/Pages/Auth/ForgotPassword.tsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import Input from '../../components/Input'
 import RoundedButton from '../../components/RoundedButton'
 import { IForgotPass } from '../../types/interfaces'
+import { useNavigate } from 'react-router-dom'
+import { useAuth } from '../../hooks/useAuth'
 const ForgotPassword = () => {
-  const [Restoptions, setResetpassword] = useState<IForgotPass>({
+  const [resetoptions, setResetpassword] = useState<IForgotPass>({
     email: '',
     newpassword: '',
   })
-
+  const navigate = useNavigate()
+  const { forgotpassword } = useAuth()
   const onChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
@@ -16,15 +19,18 @@ const ForgotPassword = () => {
     })
   }
 
-  const onSubmit = () => {}
+  const onSubmit = () => {
+    forgotpassword(resetoptions)
+    navigate('/auth/login')
+  }
 
   return (
     <div className=' bg-white h-1/2 w-3/4 md:w-1/2 lg:w-1/3 flex flex-col items-center  border-white'>
       <h3>Forgot Password</h3>
       <div className='w-full h-full flex flex-col justify-between p-5 gap-4'>
-        <Input value={Restoptions.email} label='email' onChange={onChange} />
+        <Input value={resetoptions.email} label='email' onChange={onChange} />
         <Input
-          value={Restoptions.newpassword}
+          value={resetoptions.newpassword}
           label='newpassword'
           onChange={onChange}
         />
